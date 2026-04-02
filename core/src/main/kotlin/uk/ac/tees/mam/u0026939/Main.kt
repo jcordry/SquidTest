ab78452f54d5810c9cc30a95c754af1724f886f6
JCordry <julien.cordry@gmail.com>
2026-04-02 14:07:21 +0100

    Added tileset + display

--- core/src/main/kotlin/uk/ac/tees/mam/u0026939/Main.kt
+++ core/src/main/kotlin/uk/ac/tees/mam/u0026939/Main.kt
@@ -1,13 +1,13 @@
 package uk.ac.tees.mam.u0026939
 
+import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.Texture.TextureFilter.Linear
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import com.badlogic.gdx.graphics.g2d.TextureRegion
 import ktx.app.KtxGame
 import ktx.app.KtxScreen
 import ktx.app.clearScreen
 import ktx.assets.disposeSafely
-import ktx.assets.toInternalFile
 import ktx.async.KtxAsync
 import ktx.graphics.use
 import squidpony.squidgrid.mapping.DungeonGenerator
@@ -24,34 +24,75 @@ class Main : KtxGame<KtxScreen>() {
 }
 
 class FirstScreen : KtxScreen {
-    private val image = Texture("logo.png".toInternalFile(), true).apply { setFilter(Linear, Linear) }
-    private val batch = SpriteBatch()
-
+    private lateinit var batch: SpriteBatch
+    private lateinit var tiles: Array<Array<TextureRegion>>
     private lateinit var dungeon: Array<CharArray>
+    private lateinit var camera: OrthographicCamera
+
+    fun tileIndex(c: Char): Int = when (c) {
+        '#' -> 0  // wall tile
+        '.' -> 1  // floor tile
+        '+' -> 2  // door tile
+        '/' -> 2  // diagonal door
+        '~' -> 4  // water
+        '^' -> 5  // trap
+
+        else -> 1
+    }
 
     override fun render(delta: Float) {
+        camera.update()
+        batch.projectionMatrix = camera.combined
+
         clearScreen(red = 0.7f, green = 0.7f, blue = 0.7f)
         batch.use {
-            it.draw(image, 100f, 160f)
+            for (y in dungeon.indices) {
+                for (x in dungeon[y].indices) {
+                    val region = tiles[0][tileIndex(dungeon[y][x])]
+                    val drawY = (dungeon.size - 1 - y) * 32f
+                    it.draw(region, x * 32f, drawY)
+                }
+            }
         }
 
-        dungeon.forEach {
-            println(it.concatToString())
-        }
+//        dungeon.forEach {
+//            Gdx.app.log("Open Doors", it.concatToString())
+//        }
+
+//        dungeon = DungeonUtility.openDoors(dungeon)
+//        dungeon.forEach {
+//            Gdx.app.log("Close Doors", it.concatToString())
+//        }
+
+
+
+
     }
 
     override fun dispose() {
-        image.disposeSafely()
         batch.disposeSafely()
     }
 
     override fun show() {
         super.show()
 
-        val rng = RNG()
-        val generator = DungeonGenerator(40, 25, rng)
+        val rng = RNG(1979)
+        val generator = DungeonGenerator(19, 25, rng)
+        generator.addDoors(5, true)
+        generator.addTraps(3)
+        generator.addWater(10)
 
+        // generate dungeon (SquidLib)
         dungeon = generator.generate()
         dungeon = DungeonUtility.closeDoors(dungeon)
+        batch = SpriteBatch()
+
+        val texture = Texture("colours.png")
+        tiles = TextureRegion.split(texture, 32, 32)
+
+        camera = OrthographicCamera()
+        camera.setToOrtho(false, 800f, 600f) // your window size
+
+
     }
 }
